e2b78201c71bad3c65170d4c5fd33d0e70a35d81
Dinesh P <dineshdb036@gmail.com>
2024-10-04 06:46:09 +0530

    new notes added

--- oops/CollectionAndGeneric.java
+++ oops/CollectionAndGeneric.java
@@ -1,10 +1,10 @@
 import java.util.*;//this is the package where collection and generic present
 //It can be called a Collection API or Collection FrameWork
-//Collection is usewd to store multiple values,but why we cant use arraytype because array is fixed,and we cant expand or shrink it and also it is hard to add or remove a element from a array,if we use collection API it is very easy because it has many build in class.
+//Collection is used to store multiple values,but why we cant use arraytype because array is fixed,and we cant expand or shrink it and also it is hard to add or remove a element from a array,if we use collection API it is very easy because it has many build in class.
 //Collection-Interface(singular)
 //Collections-Class(plural)
 
-//In collection there is a drawback that is it dosent support 'indexing'(we cant add a values in a particular index) and also we cant' sort, so we need to go for ArrayList (it has many methods than the Collection class)
+//In collections there is a drawback that is it dosent support 'indexing'(we cant add a values in a particular index) and also we cant' sort, so we need to go for ArrayList (it has many methods than the Collections class)
 
 public class CollectionAndGeneric {
     public static void main(String[] args) {
